273d50703e57a24fdd3223c4b9aa8babd7b8aa78
Daniel Douglas <Daniel.Douglas@seattlecolleges.edu>
2017-04-08 18:57:00 -0700

    Add files via upload

--- 1_intro_to_node/index.js
+++ 1_intro_to_node/index.js
@@ -1,32 +1,33 @@
-var http = require("http"), fs = require('fs');
+var http = require('http');
+var fs = require('fs');
 
-function serveStatic(res, path, contentType, responseCode){
-  if(!responseCode) responseCode = 200;
-  fs.readFile(__dirname + path, function(err, data){
-      if(err){
-        res.writeHead(500, {'Content-Type': 'text/plain'});
-        res.end('Internal Server Error');
-      }
-      else{
-        res.writeHead(responseCode, {'Content-Type': contentType});
-        res.end(data);
-      }
-  });
-}
+function serveStaticFile(res, path, contentType, responseCode) {
+    if(!responseCode) responseCode = 200;
+    fs.readFile(__dirname + path, function(err, data) {
+        if(err) {
+            res.writeHead(500, { 'contentType': 'text/plain'});
+            res.end('500 - Internal Error');
+        } else {
+            res.writeHead(responseCode, { 'Content-Type': contentType});
+            res.end(data); }
+        });  }
 
-http.createServer(function(req,res){
-  var path = req.url.toLowerCase();
-  switch(path) {
-    case '/': 
-      serveStatic(res, '/public/home.html', 'text/html');
-      break;
-    case '/about':
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      res.end('About');
-      break;
-    default:
-      res.writeHead(404, {'Content-Type': 'text/plain'});
-      res.end('404:Page not found.');
-  }
-  
-}).listen(process.env.PORT || 3000);
+http.createServer(function(req,res) {
+    var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+    
+    switch(path) {
+        case '':
+            serveStaticFile(res, '/public/home.html', 'text/html');
+            break;
+        
+        case '/about':
+            res.end('This app is by Daniel Douglas; it was created on the 8th of April 2017, and it is a basic server communication program.');
+            break;
+            
+        default:
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end('Not Found');
+            break; }
+    }).listen(3000);
+
+console.log("Server has started on localhost:3000; CTRL-C ends current session");
