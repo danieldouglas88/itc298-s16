d8c9dbef444e1f10b36c19ef0f653c5bda744a80
Daniel Douglas <Daniel.Douglas@seattlecolleges.edu>
2017-04-24 18:16:13 -0700

    Add files via upload

---
+++ 3_Functions/book.js
@@ -0,0 +1,25 @@
+'use strict'
+
+var books = [
+    {title: "dune", author:"frank herbert", pubdate:1969},
+    {title: "it", author:"steven king", pubdate:1975},
+    {title: "moby dick", author:"herman melville", pubdate:1869}, ];
+
+exports.get = function (para) {
+    
+    for (var i in books) { 
+    
+        if (books[i].title == para) {
+        
+            return books[i]   }  } }
+
+
+exports.del = function (para) {
+    
+    for (var i in books) { 
+    
+    if (books[i].title == para) {
+        
+        var index = books.indexOf(i);
+        books.splice(index, 1);
+        return books.length; }  } }
--- 3_Functions/index.js
+++ 3_Functions/index.js
@@ -2,55 +2,61 @@ var express = require("express");
 var app = express();
 var lead = require("../lib/lead");
 
-var back_link = "<p><a href='/'>Back</a>";
-
 // configure Express app
 app.set('port', process.env.PORT || 3000);
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/.public'));
 app.use(require("body-parser").urlencoded({extended: true}));
+
+// set template engine
+var viewsPath = __dirname + '/../views';  
+var handlebars = require('express-handlebars').create({defaultLayout: 'main', extname: '.hbs', layoutsDir: viewsPath + '/layouts',  
+  partialsDir: viewsPath + '/partials' });
+app.engine('hbs', handlebars.engine);
+app.set('views', viewsPath );
+app.set('view engine', 'hbs' );
 
 app.get('/', function(req,res){
     res.type('text/html');
-    // tell Express to use 'public' sub-folder in the week 2 directory
-    var options = { root: __dirname + '/../public/' }
-    res.sendFile('home.html', options);    
+    res.render('home', {leads: lead.getAll()} );    
+});
+
+app.get('/detail/:company', function(req,res){
+    res.type('text/html');
+    var found = lead.get(req.params.company);
+    if (!found) {
+        // note - new lead has no ID yet
+        found = {company: req.params.company};
+    }
+    res.render('detail', {lead: found} );    
 });
 
 app.get('/about', function(req,res){
-    res.type('text/plain');
-    res.send('About page' + back_link);
+    res.type('text/html');
+    res.render('about');
 });
 
 app.post('/search', function(req,res){
     res.type('text/html');
-    var header = 'Searching for: ' + req.body.company + '<br>';
-    var found = lead.find(req.body.company);
-    if (found) {
-        res.send(header + "Found: " + found.length + back_link);
-    } else {
-        res.send(header + "Not found" + back_link);
+    var found = lead.get(req.body.company);
+    if (!found) {
+        // note - new lead has no ID yet
+        found = {company: req.body.company};
     }
+    res.render('detail', {lead: found} );    
 });
 
-app.post('/add', function(req,res){
+app.post('/add', function(req,res) {
     res.type('text/html');
-    var newLead = {"company":req.body.company, "amount":req.body.amount}
-    var result = lead.add(req.body.company);
-    if (result.added) {
-        res.send("Added: " + req.body.company + "<br>New total = " + result.total + back_link);
-    } else {
-        res.send("Updated: " + req.body.company + back_link);
-    }
+    // construct new 'lead' object for comparison against existing objects
+    var newLead = {"company":req.body.company, "contact":req.body.contact, "amount":req.body.amount, "close_date":req.body.close_date}
+    var result = lead.add(newLead);
+    res.render('detail', {lead: newLead, result: result} );    
 });
 
 app.post('/delete', function(req,res){
     res.type('text/html');
     var result = lead.delete(req.body.company);
-    if (result.deleted) {
-        res.send("Deleted: " +  req.body.company + '<br>New total = ' + result.total + back_link);
-    } else {
-        res.send(req.body.company + " not found" + back_link);
-    }
+    res.render('detail', {result: result} );    
 });
 
 
