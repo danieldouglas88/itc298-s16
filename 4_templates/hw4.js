0c4f41c872c86213f6766226bc00d13932dee4ca
Daniel Douglas <Daniel.Douglas@seattlecolleges.edu>
2017-04-27 20:55:21 -0700

    Update hw4.js

--- 4_templates/hw4.js
+++ 4_templates/hw4.js
@@ -1,7 +1,7 @@
 const x = 5;
 const names = ['sara','joe','dave','ann'];
 const newArray = names.map( function(item) {
-    names.concat(x);
+    names.push(x);
     return item.toUpperCase();
  }); 
     newArray();
