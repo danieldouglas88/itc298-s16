51228a793fcbcb431a96426e1539f2eb04d307ff
Daniel Douglas <Daniel.Douglas@seattlecolleges.edu>
2017-05-24 21:28:10 -0700

    Add files via upload

---
+++ 5_rest_apis/index.js
@@ -0,0 +1,104 @@
+'use strict'
+var express = require("express");
+var app = express();
+var Book = require("./models/datamodel"); // use database model
+
+// configure Express app
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + './views'));
+app.use(require("body-parser").urlencoded({extended: true}));
+
+
+// set template engine
+let handlebars =  require("express-handlebars");
+app.engine("handlebars", handlebars({extname: 'handlebars', defaultLayout: 'main' }));
+app.set("view engine", "handlebars");
+
+app.get('/', (req,res) => {
+    Book.find((err,books) => {
+        if (err) return next(err);
+        res.render('home', {books: books, nav: '/about', whichPG: "About", whichPG2: "Home" }); 
+    })
+});
+
+
+app.get('/api/books', (req, res) => {
+	Book.getApi((err, books) => {
+		if(err){
+			throw err;
+		}
+		res.json(books);
+	});
+});
+
+app.get('/api/books/:_id', (req, res) => {
+	Book.getBookById(req.params._id, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+app.delete('/api/books/:_id', (req, res) => {
+	var id = req.params._id;
+	Book.removeBook(id, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+app.post('/api/books', (req, res) => {
+	var book = req.body;
+	Book.addBook(book, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+app.get('/about', (req,res) => {
+    res.type('text/html');
+    res.render('about',  {nav: '/', whichPG: "Home", whichPG2: "About"});
+});
+
+app.get('/get', (req,res) => {
+    Book.findOne({ title:req.query.title}, (err, books) => {
+        if (err) return next(err);
+        res.type('text/html');
+        res.render('details', {books: books, nav: '/', whichPG: "Home", whichPG2: "Details"} ); 
+    });
+});
+
+app.get('/create', (req,res) => {
+        Book.find((err,books) => {
+    new Book({title:req.query.title, author: req.query.author, pubdate: req.query.pubdate}).save();
+            res.type('text/html');
+        res.render('create', {title: req.query.title, author: req.query.author, nav: '/', whichPG: "Home", whichPG2: "Create"} ); 
+        });
+});
+
+
+app.get('/delete', (req,res) => {
+    Book.remove({ title:req.query.title }, (err, result) => {
+        if (err) return next(err);
+        let deleted = result.result.n !== 0; // n will be 0 if no docs deleted
+        Book.count((err, total) => {
+            res.type('text/html');
+            res.render('delete', {books: result, title: req.query.title, deleted: result.result.n !== 0, total: total, nav: '/', whichPG: "Home", whichPG2: "Deletion" } );    
+        });
+    });
+});
+
+app.use((req,res) => {
+    res.type('text/plain'); 
+    res.status(404);
+    res.send('404 - Not found');
+});
+
+app.listen(app.get('port'), () => {
+    console.log('Express started');    
+});
---
+++ 5_rest_apis/models/datamodel.js
@@ -0,0 +1,32 @@
+var mongoose = require('mongoose');
+
+var connectionString = 'mongodb://ddouglas88:Qazxsw!123@ds137101.mlab.com:37101/mongonode123';
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } } };
+mongoose.connect(connectionString, options);
+
+var mySchema = mongoose.Schema({
+ title: { type: String, required: true },
+ author: String,
+ pubdate: Date
+}); 
+
+var apiVar = module.exports = mongoose.model('Books', mySchema);
+
+module.exports.getApi = (callback, limit) => {
+	apiVar.find(callback).limit(limit); 
+}
+
+module.exports.getBookById = (id, callback) => {
+	apiVar.findById(id, callback);
+}
+
+module.exports.addBook = (book, callback) => {
+	apiVar.create(book);
+}
+
+module.exports.removeBook = (id, callback) => {
+	var query = {_id: id};
+	apiVar.remove(query, callback);
+}
+
+
